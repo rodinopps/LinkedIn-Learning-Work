9ac78702dab5bc590f585cd7d4bcbe3e2095c6cd
Rodin Opuz <opuzrodin@gmail.com>
2024-10-13 20:54:57 +0100

    Add files via upload

---
+++ Python Practice: Operations/string-replacement.py
@@ -0,0 +1,10 @@
+# Python code​​​​​​‌​‌‌‌‌‌​‌‌‌​‌‌‌​‌​​‌​​‌‌​ below
+# Use print("messages...") to debug your solution.
+
+show_expected_result = False
+show_hints = False
+
+# Your code goes here
+def replace_string(orig_string, old_string, new_string):
+    new_string = orig_string.replace(old_string, new_string)
+    return new_string
