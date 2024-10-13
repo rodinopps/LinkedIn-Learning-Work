4918a0601917a855cce62d2a37decc29a5937e49
Rodin Opuz <opuzrodin@gmail.com>
2024-10-13 20:55:51 +0100

    Update string-replacement.py

--- Python Practice: Operations/string-replacement.py
+++ Python Practice: Operations/string-replacement.py
@@ -6,5 +6,4 @@ show_hints = False
 
 # Your code goes here
 def replace_string(orig_string, old_string, new_string):
-    new_string = orig_string.replace(old_string, new_string)
-    return new_string
+    return orig_string.replace(old_string, new_string)
