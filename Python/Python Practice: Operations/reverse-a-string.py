c72e8bd894fededcf408fb965a9026ac616c9010
Rodin Opuz <opuzrodin@gmail.com>
2024-10-14 11:49:10 +0100

    Add files via upload

---
+++ Python/Python Practice: Operations/reverse-a-string.py
@@ -0,0 +1,8 @@
+# Python code​​​​​​‌​‌‌‌‌‌​‌‌‌​‌‌‌​‌​​‌‌‌‌‌​ below
+# Use print("messages...") to debug your solution.
+
+show_expected_result = False
+show_hints = False
+
+def reverse_string(my_string):
+    return my_string[::-1]
