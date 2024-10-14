c13eb564c36dbcb87fcdc91adeaa0c62ab8ccf04
Rodin Opuz <opuzrodin@gmail.com>
2024-10-14 11:48:49 +0100

    Delete Python/Python Practice: Operations directory

--- Python/Python Practice: Operations/are-two-strings-equal?.py
+++
@@ -1,9 +0,0 @@
-# Python code​​​​​​‌​‌‌‌‌‌​‌‌‌​‌‌‌​​‌‌‌​‌‌‌‌ below
-# Use print("messages...") to debug your solution.
-
-show_expected_result = False
-show_hints = False
-
-# Your code goes here
-def check_strings(first_string, second_string):
-    return first_string == second_string
