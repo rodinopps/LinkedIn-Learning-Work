677a31a8515c39acbc7b3cec7319649177a77d96
Rodin Opuz <opuzrodin@gmail.com>
2024-10-14 11:47:35 +0100

    Delete Python Practice: Operations directory

--- Python Practice: Operations/format-a-string.py
+++
@@ -1,8 +0,0 @@
-# Python code​​​​​​‌​‌‌‌‌‌​‌‌‌​‌‌‌​‌​‌​​‌‌‌​ below
-# Use print("messages...") to debug your solution.
-
-show_expected_result = False
-show_hints = False
-
-def calc_string(first_number, second_number):
-    return (f"The sum of {first_number} and {second_number} is {first_number + second_number}.")
