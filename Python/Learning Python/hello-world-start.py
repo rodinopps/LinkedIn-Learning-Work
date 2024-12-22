eff586b292ebc448418b46f2ac7c32b904539eaf
Rodin Opuz <opuzrodin@gmail.com>
2024-12-22 16:42:57 +0000

    Delete Python directory

--- Python/Learning Python/hello-world-start.py
+++
@@ -1,7 +0,0 @@
-def main():
-    print("Hello World!")
-    name = input("What is your name: ")
-    print(f"Nice to meet you {name.title()}.")
-
-if __name__ == "__main__":
-    main()
--- Python/Python Practice: Operations/string-replacement.py
+++
@@ -1,9 +0,0 @@
-# Python code​​​​​​‌​‌‌‌‌‌​‌‌‌​‌‌‌​‌​​‌​​‌‌​ below
-# Use print("messages...") to debug your solution.
-
-show_expected_result = False
-show_hints = False
-
-# Your code goes here
-def replace_string(orig_string, old_string, new_string):
-    return orig_string.replace(old_string, new_string)
